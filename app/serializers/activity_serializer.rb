5e895182b43d86c2941b559877b03e8dbe5740e6
Sabarinath M <sabarinath.murugesan@rootquotient.com>
2021-11-18 21:37:10 +0530

    BugFix: Activityt SHow call

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,12 +1,16 @@
 class ActivitySerializer < ActiveModel::Serializer
     attributes :id, :activity_uid, :container_number, :yard_name, :customer_name, :owner_name, \
-        :activity_type, :activity_status, :created_at, :activity_date, :sub_total
+        :activity_type, :activity_status, :created_at, :activity_date, :activity_items_subtotal
 
     belongs_to :container, serializer: ContainerSerializer
     has_many :activity_timelines, each_serializer: ActivityTimelineSerializer
 
     def created_at
         object.created_at.strftime("%d-%b-%Y")
+    end
+
+    def activity_items_subtotal
+        object.sub_total
     end
 
     puts "SERIALIZER"
