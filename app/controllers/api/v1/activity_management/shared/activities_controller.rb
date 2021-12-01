94d65372cbbbe3fab58845ec88a78ace5f9cf504
Roopali Deshpande <roopalideshpande@Roopalis-MacBook-Air.local>
2021-12-01 12:51:33 +0530

    delete activty container_status:idle

--- app/controllers/api/v1/activity_management/shared/activities_controller.rb
+++ app/controllers/api/v1/activity_management/shared/activities_controller.rb
@@ -77,6 +77,7 @@ class Api::V1::ActivityManagement::Shared::ActivitiesController < Api::V1::BaseC
 
     def delete
         @activity.update!(activity_status: 'deleted')
+        @activity.container.update!(container_status: 'idle')
     end
 
     def update_status
