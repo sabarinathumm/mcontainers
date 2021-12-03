7fd48cef566e865dcca027363dc2ae42d558fbc7
Roopali Deshpande <roopalideshpande@Roopalis-MacBook-Air.local>
2021-12-03 11:46:55 +0530

    Billing type and approval type show call updated

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
-    attributes :id, :full_name, :owner_name, :billing_name, :email, :is_active, :hourly_rate_dollars, :hourly_rate_cents, :hourly_rate_currency
+    attributes :id, :full_name, :owner_name, :billing_name, :billing_type, :approval_type, :email, :is_active, :hourly_rate_dollars, :hourly_rate_cents, :hourly_rate_currency
 
     def hourly_rate_dollars
         object.hourly_rate.dollars
